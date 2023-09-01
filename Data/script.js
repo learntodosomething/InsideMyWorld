56d3780eb664197574499ca4a45cde6677aae571
learntodosomething <121695608+learntodosomething@users.noreply.github.com>
2023-09-01 21:15:09 +0200

    Update script.js

--- Data/script.js
+++ Data/script.js
@@ -142,7 +142,7 @@ const bubble = document.createElement("div");
 bubble.classList.add("bubble");
 bubble.style = `
   --size: ${Math.random() * 4 + 2}rem;
-  --position: ${Math.random() * 100}%;
+  --position: ${Math.random() * 95}%;
   --time: ${Math.random() * 2 + 2}s;
   --delay: ${-Math.random() * 3}s;
   --distance: ${Math.random() * 6 + 6}rem;
